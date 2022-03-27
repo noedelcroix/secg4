21799dd00d808ac845917cfd75571e716f9d795e
Moi C Noé <55990@etu.he2b.be>
2022-03-27 21:48:27 +0200

    end

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # secg4
-
+## Authors :
+- 55990
+- 56212
+## How to install dependencies :
+### On Windows :
+Just type ```python -m pip install -r requirements.txt```
+### On Linux :
+Just type ```pip3 install -r requirements.txt```
+## How to run :
+### On Windows :
+Just type ```python freq.py```
+### On Linux :
+Just type ```python3 freq.py```
+## Command example :
+### Help :
+```python3 freq.py -h```
+### Encode :
+- Cesar :
+```python3 freq.py -c encode -k 4 -t test```
+- Vigenere :
+```python3 freq.py -c encode -k pass -t test```
+### Decode :
+- Cesar :
+```python3 freq.py -c decode -k 4 -t xiwx```
+- Vigenere :
+```python3 freq.py -c decode -k pass -t iekl```
+#### Attack :
+- Cesar :
+```python3 freq.py -i text.txt -c attack -a cesar```
+- Vigenere :
+```python3 freq.py -i text.txt -c attack -a vigenere```
--- freq.py
+++ freq.py
@@ -3,141 +3,218 @@
 from re import sub
 import sys
 import getopt
+import unidecode
 import math
 
-def cesar_encode(text, shift):
+
+def cesarEncode(text, shift):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    ciphered=""
+    ciphered = ""
     for letter in text:
-        ciphered+=alphabet[((ord(letter)-97)+shift)%26]
+        ciphered += alphabet[((ord(letter)-97)+shift) % 26]
     return ciphered
 
-def vigenere_encode(text, key):
-    ciphered=""
-    whitespaceCount=0
+
+def vigenereEncode(text, key):
+    ciphered = ""
+    whitespaceCount = 0
     for idx, letter in enumerate(text):
         if letter.isupper():
-            shift=ord(key[(idx-whitespaceCount)%len(key)].lower())-97
-            ascii_encode=ord(letter)+shift
-            ciphered+= chr(ascii_encode) if ascii_encode<=90 else chr(ascii_encode-26)
+            shift = ord(key[(idx-whitespaceCount) % len(key)].lower())-97
+            ascii_encode = ord(letter)+shift
+            ciphered += chr(ascii_encode) if ascii_encode <= 90 else chr(ascii_encode-26)
         elif letter.islower():
-            shift=ord(key[(idx-whitespaceCount)%len(key)].lower())-97
-            ascii_encode=ord(letter)+shift
-            ciphered+= chr(ascii_encode) if ascii_encode<=122 else chr(ascii_encode-26)
+            shift = ord(key[(idx-whitespaceCount) % len(key)].lower())-97
+            ascii_encode = ord(letter)+shift
+            ciphered += chr(ascii_encode) if ascii_encode <= 122 else chr(ascii_encode-26)
         else:
-            if(letter==" "): whitespaceCount+=1
-            ciphered+=letter
+            if(letter == " "):
+                whitespaceCount += 1
+            ciphered += letter
     return ciphered
 
 
-def cesar_decode(text, shift):
-    ciphered=""
-    whitespaceCount=0
+def cesarDecode(text, shift):
+    ciphered = ""
     for letter in text:
-       ascii_encode=ord(letter)-shift
-       ciphered+= chr(ascii_encode) if ascii_encode>=65 else chr(ascii_encode+26)
+        ascii_encode = ord(letter)-shift
+        ciphered += chr(ascii_encode) if ascii_encode >= 65 else chr(ascii_encode+26)
     return ciphered
 
-def vigenere_decode(text, key):
-    ciphered=""
-    whitespaceCount=0
+
+def vigenereDecode(text, key):
+    ciphered = ""
+    whitespaceCount = 0
     for idx, letter in enumerate(text):
         if letter.isupper():
-            shift=ord(key[(idx-whitespaceCount)%len(key)].lower())-97
-            ascii_encode=ord(letter)-shift
-            ciphered+= chr(ascii_encode) if ascii_encode>=65 else chr(ascii_encode+26)
+            shift = ord(key[(idx-whitespaceCount) % len(key)].lower())-97
+            ascii_encode = ord(letter)-shift
+            ciphered += chr(ascii_encode) if ascii_encode >= 65 else chr(ascii_encode+26)
         elif letter.islower():
-            shift=ord(key[(idx-whitespaceCount)%len(key)].lower())-97
-            ascii_encode=ord(letter)-shift
-            ciphered+= chr(ascii_encode) if ascii_encode>=97 else chr(ascii_encode+26)
+            shift = ord(key[(idx-whitespaceCount) % len(key)].lower())-97
+            ascii_encode = ord(letter)-shift
+            ciphered += chr(ascii_encode) if ascii_encode >= 97 else chr(ascii_encode+26)
         else:
-            ciphered+=letter
+            if(letter == " "):
+                whitespaceCount += 1
+            ciphered += letter
     return ciphered
 
-def findKeyLength(ciphered,length=3):
+
+def findKeyLength(ciphered, length=3):
     i = 0
     dist = []
     while i < len(ciphered):
         rep = ciphered[i:i+length]
-        if len(rep)==length: 
-            for j in range (i,len(ciphered)): 
+        if len(rep) == length:
+            for j in range(i, len(ciphered)):
                 rep2 = ciphered[j:j+length]
-                if rep==rep2:
+                if rep == rep2:
                     dist.append(j-i)
                     j = j + length + 1
-            i = i +1
-        i+=1
+            i = i + 1
+        i += 1
     dist = [i for i in dist if i != 0]
-    key = gcd(dist)
+    if(len(dist) != 0):
+        key = gcd(dist)
+    else:
+        key = 0
     if key == 1:
-        return findKeyLength(ciphered,length+1)
+        return findKeyLength(ciphered, length+1)
     else:
         return key
 
-def find_gcd(x, y):
-    x = math.gcd(x,y)
-    return x
 
 def gcd(dist):
     num1 = dist[0]
-    num2 = dist[1]
-    gcd = find_gcd(num1,num2)
-    for i in range (2,len(dist)):
-        gcd=find_gcd(gcd,dist[i])
+    if(len(dist) >= 2):
+        num2 = dist[1]
+    else:
+        num2 = dist[0]
+    gcd = math.gcd(num1, num2)
+    for i in range(2, len(dist)):
+        gcd = math.gcd(gcd, dist[i])
     return gcd
 
 
-
-ciphered =""
-with open('decode.txt','r') as f:
-    ciphered = f.read()
-    f.close()
-ciphered = ciphered.lower()
-
-
-def get_cesar_substring(ciphered, key_length):
-    substrings=[]
-    for i in range (0,key_length):
-        substring=""
-        for j in range(i,len(ciphered),key_length):
-            substring+=ciphered[j]
+def getCesarSubstring(ciphered, key_length):
+    substrings = []
+    for i in range(0, key_length):
+        substring = ""
+        for j in range(i, len(ciphered), key_length):
+            substring += ciphered[j]
         substrings.append(substring)
     return substrings
 
 
-def letter_frequency(string):
-    result=[0]*26
+def letterFrequency(string):
+    result = [0]*26
     for letter in string:
-            result[ord(letter)-97]+=1
+        result[ord(letter)-97] += 1
     return result
 
-def letter_count(frequency):
-    sum =0
+
+def letterCount(frequency):
+    sum = 0
     for value in frequency:
-        sum+=value
+        sum += value
     return sum
 
-def find_key (ciphered, key_length) :
+
+def findKey(ciphered, key_length):
     english_frequency = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772,
-                    0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978,
-                    0.02360, 0.00150, 0.01974, 0.00074]
+                         0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978,
+                         0.02360, 0.00150, 0.01974, 0.00074]
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     key = ""
-    substrings = get_cesar_substring(ciphered,key_length)
+    substrings = getCesarSubstring(ciphered, key_length)
     for substring in substrings:
-        chis=[]
-        for i in range(1,len(alphabet)):
-            frequency = letter_frequency(substring)
-            letterCpt = letter_count(frequency)
-            sumChi=0
-            for j in range (0,len(frequency)):
-                sumChi+=((frequency[j]-(letterCpt*english_frequency[j]))**2)/(letterCpt * english_frequency[j])
+        chis = []
+        for i in range(1, len(alphabet)):
+            frequency = letterFrequency(substring)
+            letterCpt = letterCount(frequency)
+            sumChi = 0
+            for j in range(0, len(frequency)):
+                sumChi += ((frequency[j]-(letterCpt*english_frequency[j]))
+                           ** 2)/(letterCpt * english_frequency[j])
             chis.append(sumChi)
-            substring = cesar_encode(substring,-1)
-        key+=alphabet[chis.index(min(chis))]
+            substring = cesarEncode(substring, -1)
+        key += alphabet[chis.index(min(chis))]
     return key
 
 
+def usage():
+    print("=============================================")
+    print("#    Cesar/Vigenere encoder and decoder.    #")
+    print("=============================================\n\n")
+    print("This tool can encode and decode text with Cesar or Vigenere algorithm. This tool can also try to find the key by analysing frequency letters.\n")
+    print("Available commands :\n")
+    print("-c, --command [required] : encode, decode or attack")
+    print("-t, --text [required or use -i] : the text that will be analysed.")
+    print(
+        "-i, --input [required or use -t] : the file containing text that will be analysed.")
+    print(
+        "-a, --algorithm [required with attack command] : specify the algorithm to attack (cesar or vigenere).")
+    print("-k, --key [required for encode and decode commands] : the key to decode/encode the text. The tool will use the Cesar encryption if this argument is digits and Vigenere if string.")
 
-print(find_key(ciphered,findKeyLength(ciphered)))
+    print("-h, --help : show this menu")
 
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "t:i:a:hk:c:", [
+                                   "help", "command", "text", "key", "input", "algorithm"])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    if len(opts) == 0:
+        usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-c", "--command"):
+            command = a.lower()
+        elif o in ("-t", "--text"):
+            text = unidecode.unidecode(a.lower())
+        elif o in ("-k", "--key"):
+            key = unidecode.unidecode(a.lower())
+        elif o in ("-i", "--input"):
+            text = unidecode.unidecode(open(a, "r").read().lower())
+        elif o in ("-a", "--algorithm"):
+            algo = unidecode.unidecode(a.lower())
+
+    if("command" in locals()):
+        if(command == "decode"):
+            if("key" in locals() and "text" in locals()):
+                if(key.isdigit()):
+                    print("The decoded text is : "+cesarDecode(text, int(key)))
+                else:
+                    print("The decoded text is : "+vigenereDecode(text, key))
+            else:
+                print("Specify the key and text parameter.")
+        elif(command == "encode"):
+            if("key" in locals() and "text" in locals()):
+                if(key.isdigit()):
+                    print("Ciphered text is : "+cesarEncode(text, int(key)))
+                else:
+                    print("Ciphered text is : "+vigenereEncode(text, key))
+            else:
+                print("Specify the key and text parameter.")
+        elif(command == "attack"):
+            if("algo" in locals() and "text" in locals()):
+                if(algo == "cesar"):
+                    print("The key is : "+findKey(text, 1))
+                elif(algo == "vigenere"):
+                    print("The key is : "+findKey(text, findKeyLength(text)))
+                else:
+                    print("Specify the algorithm parameter.")
+            else:
+                print("Specify the algo and text parameter.")
+        else:
+            print("Command not found")
+
+
+main()
